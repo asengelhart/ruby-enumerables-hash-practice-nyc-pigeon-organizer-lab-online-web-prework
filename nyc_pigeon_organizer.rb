1fc2ce3efa04c3862b01aaf20a6dd189937dbd16
Alex Engelhart <asengelhart@yahoo.com>
2019-12-09 02:55:38 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,6 +1,15 @@
 def nyc_pigeon_organizer(data)
   result = data.reduce({}) do |memo, (category, value_hash)|
     value_hash.each_pair do |entry, name_array|
-      
+      name_array.each do |name|
+        if memo[name] == nil 
+          memo[name] = {}
+        elsif memo[name][category] == nil 
+          memo[name][category] = []
+        else 
+          memo[name][category].push(entry)
+        end 
+      end 
+    end 
   end 
 end
