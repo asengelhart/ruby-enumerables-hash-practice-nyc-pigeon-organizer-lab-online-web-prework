5432b4bc48fe7addab5b0298dacacd8fb5c1c8f6
Alex Engelhart <asengelhart@yahoo.com>
2019-12-09 03:04:42 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -7,6 +7,7 @@ def nyc_pigeon_organizer(data)
         elsif memo[name][category] == nil 
           memo[name][category] = []
         else 
+          if entry.class 
           memo[name][category].push(entry)
         end 
       end 
